1d3d1a26601ffc6856327a1ee4fd9fdb38a927ed
Daniel Rodriguez <danielrs9705@gmail.com>
2020-06-29 20:03:15 +0200

    Add the fields for the form to change profile

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -3,12 +3,26 @@
  * @author Daniel Rodriguez
  */
 import React from 'react'
+import { Divider, TextField, Button } from '@material-ui/core';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import profile from '../fixtures/profile';
 
 const ProfilePage = () => {
     return (
         <div>
             <div className="content-container">
-                Profile Page
+                <div className="header">
+                    <h2>Modify Basic Information</h2>
+                </div>
+                <Divider />
+                <form className="form" autoComplete="off">
+                    <div className="form__input form__input--image">
+                        <AccountCircleIcon className="profile-photo" />
+                    </div>
+                    <TextField label="Name" variant="outlined" className="form__input" value={profile.name} />
+                    <TextField label="Bio" multiline variant="outlined" className="form__input form__input--textarea" value={profile.bio} />
+                    <Button className="button" >Save</Button>
+                </form>
             </div>
         </div>
     )
---
+++ src/fixtures/profile.js
@@ -0,0 +1,4 @@
+export default {
+    bio: "Hi, I am a Double Degree Student pursuing a master's degree in Computer Science for Computer Networks at Télécom SudParis, France and Computer Engineering degree at Universidad Simon Bolivar, Venezuela. I am very interested in Software Engineering. I was fortunate to have had industry experience early on my career. For a year, I worked as a JavaScript FullStack Developer using NodeJS and Angular 9 to build two projects: one that consists of a social network to make bets in certain events, for example soccer. This app allows you to create groups with your friends and make bets for a specific event. The second app was a system to help accountants to manage the company they are in charge of. I am currently forward to delving further into Engineering of bigger and more impactful systems for which I would like for my next projects to allow to learn more about software design, testing, and development tools.",
+    name: "Daniel Rodriguez"
+}
