c1f7de244a165543cf8a4059351d76fbfcbb94a6
Daniel Rodriguez <danielrs9705@gmail.com>
2020-07-16 17:53:42 +0200

    Change functional components by classes

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -8,50 +8,111 @@ import AddIcon from '@material-ui/icons/Add';
 import ProjectLinkForm from './ProjectLinkForm';
 import { v1 as uuid } from 'uuid'
 
-const ProjectForm = (props) => {
-    const [links, setLinks] = useState([])
+export default class ProjectForm extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props.project)
+        this.state = {
+            links: [],
+            description: props.project? props.project.description: '',
+            name: props.project? props.project.name: '',
+            date: props.project? props.project.date: ''
+        }
+    }
     /**
      * Add a new link to the form
      */
-    const handleAddLink = () => {
-        setLinks([...links, {id: uuid() }]);
+    handleAddLink = () => {
+        const links = [...this.state.links, {id: uuid()}]
+        this.setState({ links })
     }
     /**
      * Delete a link from the form
      * @param {*} id the if of the link
      */
-    const handleDeleteLink = (id) => {
-        setLinks(links.filter((link) => link.id !== id));
+    handleDeleteLink = (id) => {
+        const links = this.state.links.filter((link) => link.id !== id)
+        this.setState({ links })
     }
-    return (
-        <form className="form project">
-            <div className="project__image">
-                In here goes the image of the project
-            </div>
-            <div className="project__content">
-                <TextField label="Name" variant="outlined" className="form__input"/>
-                <TextField label="Duration" variant="outlined" className="form__input" />
-                <TextField label="Description" multiline variant="outlined" className="form__input form__input--textarea" />
-                <div className="project__links">
-                    <div className="subheader">
-                        <h4>Add Links</h4>
-                        <Fab color="primary" aria-label="add" size="small" onClick={handleAddLink} >
-                            <AddIcon fontSize="large" />
-                        </Fab>
+    render() {
+        return (
+            <form className="form project">
+                <div className="project__image">
+                    In here goes the image of the project
+                </div>
+                <div className="project__content">
+                    <TextField label="Name" variant="outlined" className="form__input" value={this.state.name}/>
+                    <TextField label="Duration" variant="outlined" className="form__input" value={this.state.date} />
+                    <TextField label="Description" multiline variant="outlined" className="form__input form__input--textarea" value={this.state.description}/>
+                    <div className="project__links">
+                        <div className="subheader">
+                            <h4>Add Links</h4>
+                            <Fab color="primary" aria-label="add" size="small" onClick={this.handleAddLink} >
+                                <AddIcon fontSize="large" />
+                            </Fab>
+                        </div>
+                        {this.state.links.map((link) => <ProjectLinkForm key={link.id} link={link} handleDeleteLink={this.handleDeleteLink} />)}
                     </div>
-                    {links.map((link) => <ProjectLinkForm key={link.id} link={link} handleDeleteLink={handleDeleteLink} />)}
-                </div>
-                <div className="project__buttons">
-                    <Button className="button button--form">
-                        Publish
-                    </Button>
-                    <Button className="button button--cancel" onClick={() => props.handleDeleteProject(props.project.id)} >
-                        Delete
-                    </Button>
-                </div>
-            </div> 
-        </form>
-    )
+                    <div className="project__buttons">
+                        <Button className="button button--form">
+                            Publish
+                        </Button>
+                        <Button className="button button--cancel" onClick={() => this.props.handleDeleteProject(this.props.project.id)} >
+                            Delete
+                        </Button>
+                    </div>
+                </div> 
+            </form>
+        )
+    }
 }
 
-export default ProjectForm
+
+// const ProjectForm = (props) => {
+//     // console.log(props.project)
+//     const [links, setLinks] = useState([])
+//     /**
+//      * Add a new link to the form
+//      */
+//     const handleAddLink = () => {
+//         setLinks([...links, {id: uuid() }]);
+//     }
+//     /**
+//      * Delete a link from the form
+//      * @param {*} id the if of the link
+//      */
+//     const handleDeleteLink = (id) => {
+//         setLinks(links.filter((link) => link.id !== id));
+//     }
+//     return (
+//         <form className="form project">
+//             <div className="project__image">
+//                 In here goes the image of the project
+//             </div>
+//             <div className="project__content">
+//                 <TextField label="Name" variant="outlined" className="form__input"/>
+//                 <TextField label="Duration" variant="outlined" className="form__input" />
+//                 <TextField label="Description" multiline variant="outlined" className="form__input form__input--textarea" />
+//                 <div className="project__links">
+//                     <div className="subheader">
+//                         <h4>Add Links</h4>
+//                         <Fab color="primary" aria-label="add" size="small" onClick={handleAddLink} >
+//                             <AddIcon fontSize="large" />
+//                         </Fab>
+//                     </div>
+//                     {links.map((link) => <ProjectLinkForm key={link.id} link={link} handleDeleteLink={handleDeleteLink} />)}
+//                 </div>
+//                 <div className="project__buttons">
+//                     <Button className="button button--form">
+//                         Publish
+//                     </Button>
+//                     <Button className="button button--cancel" onClick={() => props.handleDeleteProject(props.project.id)} >
+//                         Delete
+//                     </Button>
+//                 </div>
+//             </div> 
+//         </form>
+//     )
+// }
+
+// export default ProjectForm
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,34 +1,84 @@
 /**
  * Information about the projects of a person
  */
-import React, { useState } from 'react'
+import React from 'react'
 import { Divider, Button } from '@material-ui/core'
 import ProjectForm from './ProjectForm'
 import { v1 as uuid } from 'uuid'
+import database from '../firebase/firebase'
 
-const ProjectsPage = () => {
-    const [newProjects, setNewProjects] = useState([{id: '13as'}]);
-    const handleAddNewProject = () => {
-        setNewProjects([...newProjects, {id: uuid()}])
+export default class ProjectsPage extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            projects: []
+        }
+        this.fetchProjects();
     }
-    const handleDeleteProject = (projectId) => {
-        setNewProjects(newProjects.filter((project) => project.id !== projectId))
+
+    /**
+     * Method to fetch the projects from firebase
+     */
+    fetchProjects = () => {
+        database.ref('/projects').once('value').then((snapshot) => {
+            const projects = [];
+            snapshot.forEach((childSnapshot) => {
+                projects.push({
+                    ...childSnapshot.val(),
+                    id: childSnapshot.key
+                })
+            })
+            this.setState({ projects });
+        })
     }
-    return (
-        <div>
-            <div className="content-container">
-                <div className="header">
-                    <h2>Add Projects</h2>
+
+    handleAddNewProject = () => {
+        const projects = [...this.state.projects, {id: uuid()}]
+        this.setState({ projects })
+    }
+    handleDeleteProject = (projectId) => {
+        const projects = this.state.projects.filter((project) => project.id !== projectId)
+        this.setState({ projects })
+    }
+    render() {
+        return (
+            <div>
+                <div className="content-container">
+                    <div className="header">
+                        <h2>Add Projects</h2>
+                    </div>
+                    <Divider />
+                    {/* <ProjectForm /> */}
+                    { this.state.projects.map((project) => <ProjectForm project={project} key={project.id} handleDeleteProject={this.handleDeleteProject} />) }
+                    <Button className="button button--add" onClick={this.handleAddNewProject}>
+                        Add a new project
+                    </Button>
                 </div>
-                <Divider />
-                {/* <ProjectForm /> */}
-                { newProjects.map((project) => <ProjectForm project={project} key={project.id} handleDeleteProject={handleDeleteProject} />) }
-                <Button className="button button--add" onClick={handleAddNewProject}>
-                    Add a new project
-                </Button>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default ProjectsPage
+// const ProjectsPage = () => {
+//     const [newProjects, setNewProjects] = useState([]);
+//     // database.ref('/projects').once('value').then((snapshot) => {
+//     //     const projects = [];
+//     //     snapshot.forEach((childSnapshot) => {
+//     //         projects.push({
+//     //             ...childSnapshot.val(),
+//     //             id: childSnapshot.key
+//     //         })
+//     //     })
+//     //     setNewProjects(projects)
+//     // })
+//     console.log('Hola')
+//     const handleAddNewProject = () => {
+//         setNewProjects([...newProjects, {id: uuid()}])
+//     }
+//     const handleDeleteProject = (projectId) => {
+//         setNewProjects(newProjects.filter((project) => project.id !== projectId))
+//     }
+    
+// }
+
+// export default ProjectsPage
